b4cc75dd831ebee95a97d60802c8f9f194334fba
Jim Kang <jimkang@gmail.com>
2019-07-19 22:15:21 -0400

    Add tests for hierarchies with functions in them (using temp. local paths for dependencies).

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 test:
 	node tests/basictests.js
+	node tests/function-entity-tests.js
 
 pushall:
 	git push origin master && npm publish
---
+++ tests/function-entity-tests.js
@@ -0,0 +1,89 @@
+var test = require('tape');
+var sliceIntoLevels = require('../slice-into-levels');
+var reconstitute = require('../reconstitute');
+var getAtPath = require('../../get-at-path/index');
+
+var layoutDef = require('../../tablenest/tests/fixtures/layout-def');
+//var layoutDefLevels = require('./fixtures/layout-def-levels');
+
+var testCases = [
+  {
+    name: 'Small object with functions',
+    object: {
+      methodA: function methodA() {
+        return 1;
+      },
+      otherFns: [
+        function add(a, b) {
+          return a + b;
+        },
+        function subtract(a, b) {
+          return a - b;
+        }
+      ]
+    },
+    reconstitutedFnsToCall: [
+      { path: ['methodA'], params: [], expectedResult: 1 },
+      { path: ['otherFns', '0'], params: [2, 3], expectedResult: 5 },
+      { path: ['otherFns', '1'], params: [4, 8], expectedResult: -4 }
+    ]
+  },
+
+  {
+    name: 'Layout def with functions',
+    object: layoutDef,
+    reconstitutedFnsToCall: [
+      {
+        path: ['typeMix', '0', '1', 'fn'],
+        params: [{ size: 4, types: ['lard', 'olive oil'] }],
+        expectedResult: ['lard', 'lard', 'lard', 'lard']
+      },
+      {
+        path: ['typeMix', '2', '1', 'fn'],
+        params: [
+          { size: 4, types: ['lard', 'olive oil'] },
+          {
+            roll() {
+              return 1;
+            }
+          }
+        ],
+        expectedResult: ['olive oil', 'olive oil', 'olive oil', 'olive oil']
+      }
+    ]
+  }
+];
+
+testCases.forEach(runTest);
+
+function runTest(testCase) {
+  test(testCase.name, sliceAndFormTest);
+
+  function sliceAndFormTest(t) {
+    var levels = sliceIntoLevels(testCase.object);
+    //console.log('levels:', JSON.stringify(levels, null, 2));
+
+    var hierarchy = reconstitute(levels);
+    //console.log('hierarchy:', JSON.stringify(hierarchy, null, 2));
+    t.deepEqual(
+      hierarchy,
+      testCase.object,
+      'Object is reconstituted from levels correctly.'
+    );
+
+    testCase.reconstitutedFnsToCall.forEach(callFn);
+
+    t.end();
+
+    function callFn(fnSpec) {
+      var fn = getAtPath(hierarchy, fnSpec.path);
+      t.ok(fn, 'Function is present in reconstituted object.');
+      var result = fn.apply(fn, fnSpec.params);
+      t.deepEqual(
+        result,
+        fnSpec.expectedResult,
+        'Function works correctly after being reconstituted.'
+      );
+    }
+  }
+}
